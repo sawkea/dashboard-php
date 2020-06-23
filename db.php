638c9e709544497dffc868404efb6531f92e3d82
Sawkea <shoareau@orange.fr>
2020-06-23 14:02:55 +0200

    Update create file for connection database

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+define('DATABASE', 'dashboard');
+define('USER', 'root');
+define('PWD', '');
+define('HOST', 'localhost');
+try {
+    $dbh = new PDO('mysql:host='.HOST.';dbname='.DATABASE, USER, PWD);
+} 
+catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
