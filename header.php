ae4f219a29fa1838cfc615367bd66df57d458df8
Sawkea <shoareau@orange.fr>
2020-06-25 19:00:55 +0200

    Updates responsives

--- header.php
+++ header.php
@@ -3,14 +3,14 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Dashboard</title>
     <!-- link favicon -->
     <link rel="icon" type="image/png" href="img/favicon-dashboard.png" />
     <!-- link font -->
     <link href="https://fonts.googleapis.com/css2?family=Baloo+Chettan+2:wght@400;500&display=swap" rel="stylesheet">
     <!-- link boostrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- link styles -->
     <link rel="stylesheet" href="style.css">
 </head>
--- index.php
+++ index.php
@@ -19,47 +19,49 @@
             <h2>Listing Light Change</h2>
         </div>
             <!-- Listing of the dashboard -->
-            <table class="table">
-                <thead class="thead-dark">
-                    <tr>
-                        <td scope="col">#</td>
-                        <td scope="col">Date change</td>
-                        <td scope="col">Floor</td>
-                        <td scope="col">Location</td>
-                        <td scope="col">Power</td>
-                        <td scope="col">Brand</td>
-                        <td scope="col"></td>
-                        <td scope="col"></td>
-                    </tr>
-
-            <?php 
-                // Preparation request
-                $sql = 'SELECT id, date_change, floor, position, power, brand FROM light_change';
-                $sth = $dbh->prepare($sql);
-                // Execution request
-                $sth->execute();
-                // Recover result request
-                $result =$sth->fetchAll(PDO::FETCH_ASSOC);
-
-                // French date format management
-                $intlDateFormater = new IntlDateFormatter('fr_fr', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
-
-                // print data with foreach loop
-                foreach( $result as $row){
-                    echo '<tr>';
-                    echo '<td>'.$row['id'].'</td>';
-                    echo '<td>'.$intlDateFormater->format(strtotime($row['date_change'])).'</td>';
-                    echo '<td>'.$row['floor'].'</td>';
-                    echo '<td>'.$row['position'].'</td>';
-                    echo '<td>'.$row['power'].'</td>';
-                    echo '<td>'.$row['brand'].'</td>';
-                    echo '<td><a href="edit.php?id='.$row['id'].'"><span class="far fa-edit"></span></a></td>';
-                    echo '<td><a href="delete.php?id='.$row['id'].'" ><span class="fas fa-trash-alt"></span></a></td>';
-                    echo '<tr>';
-                }  
-                ?>
-                </thead>
-            </table>
+            <div class="table-responsive">
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <td scope="col">#</td>
+                            <td scope="col">Date change</td>
+                            <td scope="col">Floor</td>
+                            <td scope="col">Location</td>
+                            <td scope="col">Power</td>
+                            <td scope="col">Brand</td>
+                            <td scope="col"></td>
+                            <td scope="col"></td>
+                        </tr>
+    
+                <?php 
+                    // Preparation request
+                    $sql = 'SELECT id, date_change, floor, position, power, brand FROM light_change';
+                    $sth = $dbh->prepare($sql);
+                    // Execution request
+                    $sth->execute();
+                    // Recover result request
+                    $result =$sth->fetchAll(PDO::FETCH_ASSOC);
+    
+                    // French date format management
+                    $intlDateFormater = new IntlDateFormatter('fr_fr', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
+    
+                    // print data with foreach loop
+                    foreach( $result as $row){
+                        echo '<tr>';
+                        echo '<td>'.$row['id'].'</td>';
+                        echo '<td>'.$intlDateFormater->format(strtotime($row['date_change'])).'</td>';
+                        echo '<td>'.$row['floor'].'</td>';
+                        echo '<td>'.$row['position'].'</td>';
+                        echo '<td>'.$row['power'].'</td>';
+                        echo '<td>'.$row['brand'].'</td>';
+                        echo '<td><a href="edit.php?id='.$row['id'].'"><span class="far fa-edit"></span></a></td>';
+                        echo '<td><a href="delete.php?id='.$row['id'].'" ><span class="fas fa-trash-alt"></span></a></td>';
+                        echo '<tr>';
+                    }  
+                    ?>
+                    </thead>
+                </table>
+            </div>
 
             <?php 
                 // Buttons add, edit and delete 
