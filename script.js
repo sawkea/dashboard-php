aafc8ec4c28b95e7bca3d7b4d39a3847437329a8
Sawkea <shoareau@orange.fr>
2020-06-30 12:10:23 +0200

    Updates fonction modal de suppression en cours

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 //     confirmation();
 // }); 
 
-function confirmation() {
+function confirmation(argument) {
     var msg = "Are you sure you want to delete this line ?";
     if (confirm(msg))
-    location.replace('delete.php');
+    location.replace('delete.php?id=argument');
     }
     
     // function confirmation(ok) {
